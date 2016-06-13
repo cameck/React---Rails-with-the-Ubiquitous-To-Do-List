d2a1609a92bca158e72da70d7ed8d9e1f67d098c
Cameron Eckelberry <cameck@gmail.com>
2016-06-13 16:18:00 -0400

    Added Records Component

---
+++ app/assets/javascripts/records.js.jsx
@@ -0,0 +1,20 @@
+
+this.Records = React.createClass({
+  getInitialState: function() {
+    return {
+      records: this.props.data
+    };
+  },
+  getDefaultProps: function() {
+    return {
+      records: []
+    };
+  },
+  render: function() {
+    return (
+      <div className="records">
+      <h2 className="title"> Records </h2>
+      </div>
+    );
+  }
+});
