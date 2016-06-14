af91aaabab990a60eb559d125e9a014d0d77a605
Cameron Eckelberry <cameck@gmail.com>
2016-06-14 10:21:05 -0400

    Got Record Component working

--- app/assets/javascripts/components/record.js.jsx
+++ app/assets/javascripts/components/record.js.jsx
@@ -1,8 +1,14 @@
-this.Record = React.createClass({
-  render: funtion() {
-      return React.DOM.tr(null, React.DOM.td(null, this.props.record.date),
-             React.DOM.td(null, this.props.record.title),
-             React.DOM.td(null, amountFormat(this.props.record.amount))
-           );
+var Record = React.createClass({
+  render() {
+    console.log("Record");
+    return (
+
+          React.DOM.tr(null,
+            React.DOM.td(null, this.props.record.date),
+            React.DOM.td(null, this.props.record.title),
+            React.DOM.td(null, amountFormat(this.props.record.amount))
+          )
+
+      )
   }
 });
--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -1,5 +1,5 @@
 
-this.Records = React.createClass({
+var Records = React.createClass({
   getInitialState: function() {
     return {
       records: this.props.data
@@ -10,10 +10,29 @@ this.Records = React.createClass({
       records: []
     };
   },
+  returnRecord(record, i){
+    return (
+      <Record key={i} record={record}/>
+    )
+  },
   render: function() {
     return (
       <div className="records">
-      <h2 className="title"> Records </h2>
+        <h2 className="title"> Records </h2>
+        <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Title</th>
+            <th>Amount</th>
+          </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.records.map(this.returnRecord)
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
